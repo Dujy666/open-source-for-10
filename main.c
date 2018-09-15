683c2bdbcc964c572c7dc118f0d35ad25b19ad76
Dujy666 <43143296+Dujy666@users.noreply.github.com>
2018-09-16 07:57:17 +0800

    0.0.3
    normal clear

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-//clear all original output, and when you enter 0 return menu
+//normal clear
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,75 +6,74 @@
 
 void Interface(void)
 {
-	printf("-----------------Lottery-------------------\n");
-	printf("                 1.Rules                  \n");
-	printf("                 2.Sign Up                \n");
-	printf("                 3.Check                  \n");
-	printf("                 4.Clear                  \n");
-	printf("                 5.Star                   \n");
-	printf("                 6.Result                 \n");
-	printf("                 7.Exit                   \n");
-	printf("-------------------------------------------\n");
+    printf("---------------Lottery Program------------\n");
+    printf("                 1.Rules                  \n");
+    printf("                 2.Sign Up                \n");
+    printf("                 3.Check                  \n");
+    printf("                 4.Clear                  \n");
+    printf("                 5.Star                   \n");
+    printf("                 6.Result                 \n");
+    printf("                 7.Exit                   \n");
+    printf("------------------------------------------\n");
 }
 
 void Display_Rules(void)
 {
-	printf(".................\n");
-	printf(".................\n");
-	printf(".................\n");
+    printf(".................\n");
+    printf(".................\n");
+    printf(".................\n");
 }
 
 void Sign_Up(void)
 {
-	printf(".................\n");
-	printf(".................\n");
-	printf(".................\n");
+    printf(".................\n");
+    printf(".................\n");
+    printf(".................\n");
 }
 
 void Check(void)
 {
-	printf(".................\n");
-	printf(".................\n");
-	printf(".................\n");
+    printf(".................\n");
+    printf(".................\n");
+    printf(".................\n");
 }
 
 void Quit(void)
 {
-	printf("You have exit the program!\n");
+    printf("You have exit the program!\n");
 }
 
-void Choose_Interface(int enter)
+void Choose_Interface(void)
 {
-	int enter2;
-	while(1){
-		switch (enter)
-        	{
-			case 1: Display_Rules();
-				break;
-			case 2: Sign_Up();
-				break;
-			case 3: Check();
-				break;
-			default:
-				break;
-		}
-		system("clear");
-		scanf("%d", &enter2);
-		if(enter2 == 0)
-			Interface();
-		break;
+    int choice1, choice2;
+    while (1)
+    {
+        scanf("%d", &choice1);
+        system("clear");
+        switch (choice1)
+        {
+            case 1: Display_Rules();
+                    break;
+            case 2: Sign_Up();
+                    break;
+            case 3: Check();
+                break;
         }
+        scanf("%d", &choice2);
+        if(choice2 == 0)
+        {
+            system("clear");
+            Interface();
+        }
+        else
+            break;
+    }
 }
 
 int main()
 {
-	Interface();
+    Interface();
+    Choose_Interface();
 
-	int enter;
-	scanf("%d", &enter);
-	Choose_Interface(enter);
-
-	return 0;
+    return 0;
 }
-
-
